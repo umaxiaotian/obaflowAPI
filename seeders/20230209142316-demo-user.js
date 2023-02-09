b313b8390b4a1ea34d5474fcac3642235d784b60
小畑佑馬 <yuumaobata1008@outlook.com>
2023-02-10 01:22:32 +0900

    SEEDにHASHPASSを追加

--- seeders/20230209142316-demo-user.js
+++ seeders/20230209142316-demo-user.js
@@ -5,7 +5,8 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
       username:'admin',
-      password:'password',
+      //↓パスワード：password
+      password:'e9253671ba39ab34f6a2470e79dbaaabcb27022daec0c6c2748683522928ea96bf0797e19ef412f7d259329371f8305741960dddab0af3355a865006800bf6ec',
       firstName: 'test',
       lastName: 'test',
       email: 'example@example.com',
